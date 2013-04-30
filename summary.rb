39459ff87d4b5fefe61014fbb22027c5f68fc21c
Indraniel Das <idas@genome.wustl.edu>
2013-04-30 18:28:46 -0500

    + further work on summary.rb

--- summary.rb
+++ summary.rb
@@ -10,8 +10,8 @@ module ConsoleTools
     attr_accessor :count, :min, :max, :mean, :variance, :kurtosis, :skew, :quantiles
 
     def initialize(quantiles=[0.5])
-      @max      = (2**(0.size * 8 -2) -1)  # ruby system FIXNUM_MAX
-      @min      = -(2**(0.size * 8 -2))    # ruby system FIXNUM_MIN
+      @min      = (2**(0.size * 8 -2) -1)  # ruby system FIXNUM_MAX
+      @max      = -(2**(0.size * 8 -2))    # ruby system FIXNUM_MIN
       @count    = 0
       @mean     = 0.0
       @variance = 0.0
@@ -24,6 +24,9 @@ module ConsoleTools
 
       @count += 1
 
+      @min = data < @min ? data : @min
+      @max = data > @max ? data : @max
+
       # Sample average / mean
       @mean = (@count * @mean + data) / (@count + 1)
 
@@ -34,21 +37,28 @@ module ConsoleTools
       @kurtosis = @kurtosis = (data - @mean)**4.0
 
       # Sample skewness (except for the scaling term)
-      @skew = @skew + (item - @mean)**3.0
+      @skew = @skew + (data - @mean)**3.0
     end
 
-    def show()
+    def show
+      puts "%-10s : %10d" % ["# records", @count]
+      puts "%-10s : %10f" % ["Min", @min]
+      puts "%-10s : %10f" % ["Max", @max]
+      puts "%-10s : %10f" % ["mean", @mean]
+      puts "%-10s : %10f" % ["variance", self.sample_variance]
+      puts "%-10s : %10f" % ["skew", self.sample_skew]
+      puts "%-10s : %10f" % ["kurtosis", self.sample_kurtosis]
     end
 
-    def sample_variance()
+    def sample_variance
       return @variance / (@count - 1)
     end
 
-    def sample_kurtosis()
+    def sample_kurtosis
       return (@kurtosis / (@count * @variance**2.0)) - 3.0
     end
 
-    def sample_skew()
+    def sample_skew
       return @skew / (@count * @variance**1.5)
     end
 
@@ -59,7 +69,9 @@ if __FILE__ == $0
   options = {}
 
   optparse = OptionParser.new do |opts|
-    opts.on('-t', '--tiles x,y,z', 'list tiles of interest (default: "0.25,0.5,0.75")') do |list|
+    options[:tiles] = [0.25, 0.5, 0.75]
+    description = 'list tiles of interest (default: "0.25,0.5,0.75")'
+    opts.on('-t', '--tiles x,y,z', Array, description) do |list|
       list.map! { |i| i.to_f }
       options[:tiles] = list 
     end
