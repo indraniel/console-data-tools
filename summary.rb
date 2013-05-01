d427ba32f95404feecf3b0028f5278c6c07cedd1
Indraniel Das <idas@genome.wustl.edu>
2013-05-01 16:39:29 -0500

    + more development work on summary.rb

--- summary.rb
+++ summary.rb
@@ -6,6 +6,88 @@
 require 'optparse'
 
 module ConsoleTools
+  class EstimatedQuantile
+    # This class is designed to work for large numerical datasets.
+    #
+    # Class instances estimate a given quantile by calculating them
+    # dynamically as data points are observed, via a set of 5 statistical
+    # counters. IT DOES NOT STORE ANY OF THE OBSERVATIONS.
+    #
+    # It is based upon the following paper
+    # "The P^2 Algorithm for Dynamic Calculation of Quantiles and Histograms
+    #  Without Storing Observations"
+    # see http://www.cs.wustl.edu/~jain/papers/ftp/psqr.pdf
+    attr_accessor :quantile, :desired_positions, :positions, :heights, :startup,
+                  :delta_positions
+
+    def initialize(quantile=0.5)
+      @quantile  = quantile
+      @heights   = Array.new
+      @positions = (1..5).to_a
+      @desired_positions = [1, 1 + 2*quantile, 1 + 4*quantile, 3 + 2*quantile, 5]
+      @delta_positions = [0, quantile/2, quantile, (1 + quantile)/2, 1]
+      @startup   = false
+    end
+
+    def record(data)
+      # collect the 1st 5 observations
+      if @heights.length != 5
+        @heights.push(data)
+        return 1
+      end
+
+      # sort the 1st 5 observations
+      # (only for the first time we get to this section of code)
+      if @startup == false
+        @heights.sort!
+        @startup = true
+      end
+
+      k = 0
+
+      # data observation is the lowest thing seen so far
+      if data < @heights[0]
+        @heights[0] = data
+        k = 1
+
+      # check if data observation is in the range of middle three markers
+      else
+        (1...@heights.length).to_a.each do |i|
+          if @heights[i-1] <= data && data < @heights[i]
+            k = i
+            break
+          end
+        end
+      end
+
+      # if the data observation is the highest thing seen so far
+      if k == 0 
+        k = 4
+        @heights[-1] = data if @heights[-1] < data
+      end
+
+      # increment all the positions 
+
+    end
+
+    def p2(qp1, q, qm1, d, np1, n, nm1)
+      c1 = d / (np1 - nm1)
+      c2 = (n - nm1 + d) * (qp1 - q) / (np1 - n)
+      c3 = (np1 - n - d) * (q - qm1) / (n - nm1)
+
+      q_new = q + c1 * (c2 + c3)
+      return q_new
+    end
+
+    def quantile
+    end
+
+    private
+    def adjust_markers
+    end
+
+  end
+
   class SummaryStats
     attr_accessor :count, :min, :max, :mean, :variance, :kurtosis, :skew, :quantiles
 
